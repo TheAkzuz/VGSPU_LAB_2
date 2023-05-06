1f6911a0aa5d1f329bcfac68d9925c443552d12d
TheAkzuz <131234047+TheAkzuz@users.noreply.github.com>
2023-05-06 10:51:11 +0300

    Add files via upload

---
+++ lab_2_4/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab_2_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 0;
+            Console.Write("Ведите целое число:");
+            a = int.Parse(Console.ReadLine());
+            string b = a.ToString();
+            int s = b.Length;
+            if (b[s - 1] == '7')
+            {
+                Console.WriteLine("Число оканчивается на 7");
+            }
+            else
+            {
+                Console.WriteLine("Число не оканчивается на 7");
+            }
+        }
+    }
+}
